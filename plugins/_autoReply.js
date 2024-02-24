36393e4ca0afd15b16b26576aa8446cd5b19d42b
Goddy234 <153915993+Goddy234@users.noreply.github.com>
2024-02-24 15:29:01 +0100

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -1,7 +1,7 @@
 export async function all(m) {
 	
   // when someone sends a group link to the bot's dm
-  if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
+  if ((m.mtype === 'Gain massive contacts with Goddy's Hub gain train 🚂🚉Join batch 4️⃣ verified contacts file listDo not miss this opportunity to have more viewers on your WhatsApp status💯💯More potential customers buying from you  verify before Batch 4️⃣ ends soonhttps://chat.whatsapp.com/CRhVEA4Kp7V2UzVsxSNQrp' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
    this.sendMessage(m.chat,{text:`Hello @${m.sender.split('@')[0]}\nyou can rent the bot to join a group\n\n_For more info you can DM the owner_\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim()}, {quoted:m});
    /*this.sendButton(m.chat, `*Invite bot to a group*      
     Hallo @${m.sender.split('@')[0]} 
